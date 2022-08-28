8866e8f39ae46603930da0816f911e87e929d236
caesar003 <caesarmuksid@gmail.com>
2022-08-28 10:01:27 -0700

    stacked list component

---
+++ src/Example.js
@@ -0,0 +1,55 @@
+const champions = [
+    {
+        name: "Anatoly Karpov",
+        nationality: "Russia",
+        year: "1993-1999",
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Anatoly_Karpov_2017_april.jpg",
+    },
+    {
+        name: "Alexander Khalifman",
+        nationality: "Russia",
+        year: "1999-2000",
+        image: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Alexander_Khalifman.jpg",
+    },
+    {
+        name: "Viswanathan Anand",
+        nationality: "India",
+        year: "2000-2002",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/57/Viswanathan_Anand_%282016%29_%28cropped%29.jpeg"
+    },
+    {
+        name: "Ruslan Ponomariov",
+        nationality: "Ukraine",
+        year: "2002-2004",
+        image: "https://upload.wikimedia.org/wikipedia/commons/b/b6/RPonomariov.jpg",
+    },
+    {
+        name: "Rustam Kasimdzhanov",
+        nationality: "Uzbekistan",
+        year: "2004-2005",
+        image: "https://upload.wikimedia.org/wikipedia/commons/1/12/Kasimdhzanov_Torino_2006.JPG",
+    },
+    {
+        name: "Veselin Topalov",
+        nationality: "2005-2006",
+        year: "Bulgaria",
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/97/Veselin_Topalov_2013.jpg",
+    },
+
+];
+
+export default function Example () {
+    return (
+        <ul className="divide-y divide-gray-200">
+           {champions.map((champion) => (
+            <li key={champion.name} className="py-4 flex">
+                <img className="h-10 w-10 rounded-full" src={champion.image} alt={champion.name}/>
+                <div className="ml-3">
+                    <p className="text-sm font-medium text-gray-900">{champion.name} - {champion.year}</p>
+                    <p className="text-sm text-gray-500">{champion.nationality}</p>
+                </div>
+            </li>
+           ))}
+        </ul>
+    )
+}
